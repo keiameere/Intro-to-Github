bffdd3b644df0a50dd4d1c3c3744635f8fb81057
Keia <keiameereayesatucker@gmail.com>
2026-09-25 19:22:47 +0800

    Addtional files uploaded

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <?php
+        //Sample Scripts
+        //Script name: SampleOutput_1.php
+        //Output #1: Without input validation
+
+        echo "OUTPUT WITHOUT VALIDATION<br><br>";
+        echo "<b>Name: </b>" . $_POST['txtName'] . "<br>";
+        echo "<b>Age: </b>" . $_POST['txtAge'] . "<br>";
+        echo "<b>Phone: </b>" . $_POST['txtPhone'] . "<br>";
+        echo "<b>Bill: </b>" . $_POST['txtBill'] . "<br>";
+        ?>	
+    </body>
+</html>
